88dcfba7061cf57e1840826eedbcd0db5a723d33
agonzalez97 <aidamaria.gonzalez@dxc.com>
2023-02-22 18:47:54 +0100

    template

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RoomsComponent } from './components/rooms/rooms.component';
+
+const routes: Routes = [{ path: '', component: RoomsComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
